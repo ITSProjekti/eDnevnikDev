2c13e70c2e60eedec4d4dff3549fda61e9a32e43
Filip/Firas <firas35615@its.edu.rs>
2017-05-23 15:54:10 +0200

    Dodat atribut Required za sva polja klase Ucenik

--- eDnevnikDev/Models/Ucenik.cs
+++ eDnevnikDev/Models/Ucenik.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,18 @@ namespace eDnevnikDev.Models
     public class Ucenik
     {
         public int UcenikID { get; set; }
+
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Polje za ime je obavezno")]
         public string Ime { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Polje za prezime je obavezno")]
         public string Prezime { get; set; }
         public string JMBG { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Polje za adresu je obavezno")]
         public string Adresa { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Polje za roditelja/staratelja je obavezno")]
         public string RoditeljStaratelj { get; set; }
 
     }
