00a5005e034cf7a3861fa1434e2a6a39f9fdb472
Student <Student@ADMINRG-CDNBDLD>
2017-05-23 14:36:00 +0200

    Add project files.

---
+++ eDnevnikDev/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eDnevnikDev.MvcApplication" Language="C#" %>
---
+++ eDnevnikDev/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eDnevnikDev.Startup))]
+namespace eDnevnikDev
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
