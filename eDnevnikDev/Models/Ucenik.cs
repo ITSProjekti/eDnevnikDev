8f5a6e562e2aaff003dbb7ed143a89f9b685d918
Firas/Filip <firas35615@its.edu.rs>
2017-05-24 15:35:38 +0200

    Dodato Polje za adresu sa validacijom. Dodato polje za Roditelja/staratelja sa validacijom

--- eDnevnikDev/Models/Ucenik.cs
+++ eDnevnikDev/Models/Ucenik.cs
@@ -16,14 +16,17 @@ namespace eDnevnikDev.Models
         [Required(AllowEmptyStrings = false, ErrorMessage = "Polje za prezime je obavezno")]
         public string Prezime { get; set; }
 
-        
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Polje za JMBG je obavezno")]
         [RegularExpression("(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[012])[0|9][0-9]{2}[0-9]{6}", ErrorMessage ="JMBG nije ispravan")]
         public string JMBG { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Polje za adresu je obavezno")]
+        [RegularExpression(@"^[A-Za-z0-9'\.\-\s\,]+$",ErrorMessage ="Nisu dozoljeni specijalni karakteri")]
         public string Adresa { get; set; }
 
+        [Display(Name = "Roditelj")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Polje za roditelja/staratelja je obavezno")]
+        [RegularExpression(@"^([A-ZŠĐČĆŽ]{1}[a-zšđčćž]+ ?)+$", ErrorMessage = "Ime roditelja nije ispravno (Prvo slovo mora biti veliko)")]
         public string RoditeljStaratelj { get; set; }
 
     }
