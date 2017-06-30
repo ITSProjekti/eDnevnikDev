112b53f61699f33c6cb1011b71a54cdf89cbbae4
Filip <nikola7015@its.edu.rs>
2017-06-30 10:56:22 +0200

    Dodat dropdown za predmet(mora da se doradi)

--- eDnevnikDev/Controllers/EvidencijaController.cs
+++ eDnevnikDev/Controllers/EvidencijaController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using eDnevnikDev.Models;
 
 namespace eDnevnikDev.Controllers
 {
     public class EvidencijaController : Controller
     {
+        ApplicationDbContext _context;
+        public EvidencijaController()
+        {
+            _context = new ApplicationDbContext();
+        }
         // GET: Evidencija
         public ActionResult Index()
         {
-            return View();
+            var profesor = _context.Profesori.SingleOrDefault(p => p.ProfesorID == 1);
+            return View(profesor);
         }
     }
 }
