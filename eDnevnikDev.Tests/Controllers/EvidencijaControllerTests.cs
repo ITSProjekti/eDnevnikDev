91648f20f41cdcdf756c851f590648b0854c920b
Vedran <vedran19215@its.edu.rs>
2017-10-10 13:57:05 +0200

    testDone

--- eDnevnikDev.Tests/Controllers/EvidencijaControllerTests.cs
+++ eDnevnikDev.Tests/Controllers/EvidencijaControllerTests.cs
@@ -38,10 +38,9 @@ namespace eDnevnikDev.Tests.Controllers
             var tipOcena = new List<TipOcene> {
                 new TipOcene { TipOceneId=1,Tip="kontrolni"},
                 new TipOcene { TipOceneId=2,Tip="pismeni" }}.AsQueryable();
-
             var ocene = new List<Ocena> {
-                new Ocena { CasId = 1, Oznaka = 5, Napomena = "napomena 1", TipOceneId = 2,UcenikId=1 },
-                new Ocena { CasId=1,Oznaka=4,Napomena="napomena 2",TipOceneId=1,UcenikId=1 } }.AsQueryable();
+                new Ocena { CasId = 1, Oznaka = 5, Napomena = "napomena 1", TipOceneId = 2,TipOcene=tipOcena.Where(x=>x.TipOceneId==2).First(), UcenikId=1 },
+                new Ocena { CasId=1,Oznaka=4,Napomena="napomena 2",TipOceneId=1,UcenikId=1,TipOcene=tipOcena.Where(x=>x.TipOceneId==1).First() } }.AsQueryable();
             
 
             var mockContext = new Mock<ApplicationDbContext>();
@@ -54,17 +53,17 @@ namespace eDnevnikDev.Tests.Controllers
             mockContext.Setup(x => x.Casovi).Returns(mockSetCas.Object);
             //Mock ocene
             var mockSetOcene = new Mock<DbSet<Ocena>>();
-            mockSetCas.As<IQueryable<Ocena>>().Setup(x => x.Provider).Returns(ocene.Provider);
-            mockSetCas.As<IQueryable<Ocena>>().Setup(x => x.Expression).Returns(ocene.Expression);
-            mockSetCas.As<IQueryable<Ocena>>().Setup(x => x.ElementType).Returns(ocene.ElementType);
-            mockSetCas.As<IQueryable<Ocena>>().Setup(x => x.GetEnumerator()).Returns(ocene.GetEnumerator());
+            mockSetOcene.As<IQueryable<Ocena>>().Setup(x => x.Provider).Returns(ocene.Provider);
+            mockSetOcene.As<IQueryable<Ocena>>().Setup(x => x.Expression).Returns(ocene.Expression);
+            mockSetOcene.As<IQueryable<Ocena>>().Setup(x => x.ElementType).Returns(ocene.ElementType);
+            mockSetOcene.As<IQueryable<Ocena>>().Setup(x => x.GetEnumerator()).Returns(ocene.GetEnumerator());
             mockContext.Setup(x => x.Ocene).Returns(mockSetOcene.Object);
             //Mock tip ocene
             var mockSetTipOcene = new Mock<DbSet<TipOcene>>();
-            mockSetOcene.As<IQueryable<TipOcene>>().Setup(x => x.Provider).Returns(tipOcena.Provider);
-            mockSetOcene.As<IQueryable<TipOcene>>().Setup(x => x.Expression).Returns(tipOcena.Expression);
-            mockSetOcene.As<IQueryable<TipOcene>>().Setup(x => x.ElementType).Returns(tipOcena.ElementType);
-            mockSetOcene.As<IQueryable<TipOcene>>().Setup(x => x.GetEnumerator()).Returns(tipOcena.GetEnumerator());
+            mockSetTipOcene.As<IQueryable<TipOcene>>().Setup(x => x.Provider).Returns(tipOcena.Provider);
+            mockSetTipOcene.As<IQueryable<TipOcene>>().Setup(x => x.Expression).Returns(tipOcena.Expression);
+            mockSetTipOcene.As<IQueryable<TipOcene>>().Setup(x => x.ElementType).Returns(tipOcena.ElementType);
+            mockSetTipOcene.As<IQueryable<TipOcene>>().Setup(x => x.GetEnumerator()).Returns(tipOcena.GetEnumerator());
             mockContext.Setup(x => x.TipoviOcena).Returns(mockSetTipOcene.Object);
 
             var controller = new EvidencijaController(mockContext.Object);
@@ -78,33 +77,18 @@ namespace eDnevnikDev.Tests.Controllers
             System.Diagnostics.Debug.WriteLine(jsonString);
 
             var jsonArray = JArray.Parse(Helper.checkJsonJArray(jsonString));
+            int i = 0;
             foreach (JObject item in jsonArray)
             {
-                foreach(JObject item2 in item)
-                {
-
-                }
-                var napomenaId = item["NapomenaId"].ToString();
-                var opis = item["Opis"].ToString();
-                var _ucenikId = item["UcenikId"].ToString();
-                var profesorId = item["ProfesorId"].ToString();
-                var _casId = item["CasId"].ToString();
-
-
-
-                Napomena temp = listNapomena.FirstOrDefault(x => x.NapomenaId == int.Parse(napomenaId));
-                Assert.AreEqual<string>(napomenaId, temp.NapomenaId.ToString());
-                Assert.AreEqual<string>(opis, temp.Opis.ToString());
-                Assert.AreEqual<string>(_ucenikId, temp.UcenikId.ToString());
-                Assert.AreEqual<string>(profesorId, temp.ProfesorId.ToString());
-                Assert.AreEqual<string>(_casId, temp.CasId.ToString());
-                Assert.AreEqual<int>(jsonArray.Count(), 1);
-
-
+                var ocena = item["Ocena"].ToString();
+                var tipOcene = item["TipOcene"].ToString();
+                var komentar = item["Komentar"].ToString();
+                Ocena temp = ocene.ElementAtOrDefault(i++);
+                Assert.AreEqual(ocena, temp.Oznaka.ToString());
+                Assert.AreEqual(tipOcene, temp.TipOcene.Tip.ToString());
+                Assert.AreEqual(komentar, temp.Napomena.ToString());
             }
-
-
-
+            Assert.AreEqual(jsonArray.Count(), 2);
 
         }
     }
