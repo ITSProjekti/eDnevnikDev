0651713ae0a67c9c53ca8588941ab46f9fd7f0ee
Vedran <vedran19215@its.edu.rs>
2017-10-17 13:51:22 +0200

    OneMoreTestDoneDusan

--- eDnevnikDev.Tests/Controllers/RoleControllerTests.cs
+++ eDnevnikDev.Tests/Controllers/RoleControllerTests.cs
@@ -20,6 +20,7 @@ using eDnevnikDev.Tests.Helpers;
 using System.Web;
 using System.Security.Principal;
 using Microsoft.AspNet.Identity;
+using System.Net;
 
 namespace eDnevnikDev.Tests.Controllers
 {
@@ -29,28 +30,6 @@ namespace eDnevnikDev.Tests.Controllers
     [TestClass]
     public class RoleControllerTests
     {
-        //public static Mock<SignInManager<TUser>> MockSignInManager<TUser>() where TUser : class
-        //{
-        //    var context = new Mock<HttpContext>();
-        //    var manager = MockUserManager<TUser>();
-        //    return new Mock<SignInManager<TUser>>(manager.Object,
-        //        new HttpContextAccessor { HttpContext = context.Object },
-        //        new Mock<IUserClaimsPrincipalFactory<TUser>>().Object,
-        //        null, null)
-        //    { CallBase = true };
-        //}
-
-        //public static Mock<UserManager<TUser>> MockUserManager<TUser>() where TUser : class
-        //{
-        //    IList<IUserValidator<TUser>> UserValidators = new List<IUserValidator<TUser>>();
-        //    IList<IPasswordValidator<TUser>> PasswordValidators = new List<IPasswordValidator<TUser>>();
-
-        //    var store = new Mock<IUserStore<TUser>>();
-        //    UserValidators.Add(new UserValidator<TUser>());
-        //    PasswordValidators.Add(new PasswordValidator<TUser>());
-        //    var mgr = new Mock<UserManager<TUser>>(store.Object, null, null, UserValidators, PasswordValidators, null, null, null, null, null);
-        //    return mgr;
-        //}
 
         Mock<ApplicationDbContext> mockContext;
         RoleController controler;
@@ -127,6 +106,104 @@ namespace eDnevnikDev.Tests.Controllers
             }
             Assert.AreEqual(jsonArray.Count(), 3);
 
+
+        }
+
+        [TestMethod]
+        public void VratiUcenike()
+        {
+            var ucenici = new List<Ucenik>
+            {
+                new Ucenik() { UcenikID = 1, UserUcenikId="13214332", Ime="Ucenik",Prezime="Ucenicic",JMBG="1231243214"},
+                new Ucenik() { UcenikID = 2,UserUcenikId="43535312", Ime="Ucenik2",Prezime="Ucenicic",JMBG="4523322413"},
+                new Ucenik() { UcenikID = 3,UserUcenikId="63242234", Ime="Ucenik3",Prezime="Ucenicic",JMBG="6575612134" }
+
+            }.AsQueryable();
+            var role = new List<string>()
+            {
+                "Ucenik",
+                "Admin"
+            } as IList<string>;
+            var mockSetUcenik = new Mock<DbSet<Ucenik>>();
+            mockSetUcenik.As<IQueryable<Ucenik>>().Setup(m => m.Provider).Returns(ucenici.Provider);
+            mockSetUcenik.As<IQueryable<Ucenik>>().Setup(m => m.Expression).Returns(ucenici.Expression);
+            mockSetUcenik.As<IQueryable<Ucenik>>().Setup(m => m.ElementType).Returns(ucenici.ElementType);
+            mockSetUcenik.As<IQueryable<Ucenik>>().Setup(m => m.GetEnumerator()).Returns(ucenici.GetEnumerator());
+            mockContext.Setup(x => x.Ucenici).Returns(mockSetUcenik.Object);
+
+            var userStore = new Mock<IUserStore<ApplicationUser>>();
+            var appUserManager = new Mock<ApplicationUserManager>(userStore.Object);
+            appUserManager.Setup(x => x.GetRolesAsync(It.IsAny<string>())).ReturnsAsync(role);
+            controler._userManager = appUserManager.Object;
+            var result = controler.VratiUcenike() as Task<JsonResult>;
+
+            //Assert result
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Result.JsonRequestBehavior == JsonRequestBehavior.AllowGet);
+
+            string jsonString = JsonConvert.SerializeObject(result.Result.Data);
+            System.Diagnostics.Debug.WriteLine(jsonString);
+
+            var jsonArray = JArray.Parse(Helper.checkJsonJArray(jsonString));
+            var i = 0;
+            foreach (JObject item in jsonArray)
+            {
+
+                var id = item["Id"].ToString();
+                var jmbg = item["JMBG"].ToString();
+                var ime = item["Ime"].ToString();
+                var prezime = item["Prezime"].ToString();
+                var roleTemp = item.GetValue("Role");
+                List<string> listaRola = new List<string>();
+                foreach (JValue itemInner in roleTemp)
+                {
+                    listaRola.Add(itemInner.ToString());
+                }
+                Ucenik temp = ucenici.ElementAtOrDefault(i++);
+                Assert.AreEqual(id, temp.UserUcenikId);
+                Assert.AreEqual(jmbg, temp.JMBG);
+                Assert.AreEqual(ime, temp.Ime);
+                Assert.AreEqual(prezime, temp.Prezime);
+                Assert.AreEqual(listaRola[0], role[0].ToString());
+                Assert.AreEqual(listaRola[1], role[1].ToString());
+
+            }
+            Assert.AreEqual(jsonArray.Count(), 3);
+        }
+        [TestMethod]
+        public void PromeniPravoPristupaProfesora()
+        {
+            string id = "22222222";
+            var profesori = new List<Profesor>
+            {
+                new Profesor() { ProfesorID = 1, Ime="Profesor",Prezime="Profesoric",Adresa="Adresa 1", Telefon="0654182374",Vanredan = true,UserProfesorId="1111111"},
+                new Profesor() { ProfesorID = 2, Ime="Profesor2",Prezime="Profesoric2",Adresa="Adresa 12", Telefon="767894272",Vanredan = true,UserProfesorId="22222222" },
+                new Profesor() { ProfesorID = 3, Ime="Profesor3",Prezime="Profesoric3",Adresa="Adresa 123", Telefon="0239042322",Vanredan = true,UserProfesorId="33333333" }
+
+            }.AsQueryable();
+            var role = new List<string>()
+            {
+                "Profesor",
+                "Admin",
+            } as IList<string>;
+
+            var mockSetProfesor = new Mock<DbSet<Profesor>>();
+            mockSetProfesor.As<IQueryable<Profesor>>().Setup(m => m.Provider).Returns(profesori.Provider);
+            mockSetProfesor.As<IQueryable<Profesor>>().Setup(m => m.Expression).Returns(profesori.Expression);
+            mockSetProfesor.As<IQueryable<Profesor>>().Setup(m => m.ElementType).Returns(profesori.ElementType);
+            mockSetProfesor.As<IQueryable<Profesor>>().Setup(m => m.GetEnumerator()).Returns(profesori.GetEnumerator());
+            mockContext.Setup(x => x.Profesori).Returns(mockSetProfesor.Object);
+
+            var userStore = new Mock<IUserStore<ApplicationUser>>();
+            var appUserManager = new Mock<ApplicationUserManager>(userStore.Object);
+            appUserManager.Setup(x => x.GetRolesAsync(It.IsAny<string>())).ReturnsAsync(role);
+            controler._userManager = appUserManager.Object;
+            var result = controler.VratiUcenike() as Task<JsonResult>;
+
+            //Assert
+            var resultNullId = controler.PromeniPravoPristupaProfesora(null);
+            Assert.AreEqual((HttpStatusCodeResult)resultNullId, new HttpStatusCodeResult(HttpStatusCode.BadRequest));
+            //var result = controler.PromeniPravoPristupaProfesora(id);
 
         }
     }
