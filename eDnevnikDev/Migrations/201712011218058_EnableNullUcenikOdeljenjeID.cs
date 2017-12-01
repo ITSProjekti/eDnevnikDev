bf30f36715f2d7883cb93519903dba6a34421db1
Firas/Filip <firas35615@its.edu.rs>
2017-12-01 13:19:41 +0100

    Dodat null u uceniku za odljenje.

---
+++ eDnevnikDev/Migrations/201712011218058_EnableNullUcenikOdeljenjeID.cs
@@ -0,0 +1,26 @@
+namespace eDnevnikDev.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EnableNullUcenikOdeljenjeID : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Ucenik", "OdeljenjeId", "dbo.Odeljenje");
+            DropIndex("dbo.Ucenik", new[] { "OdeljenjeId" });
+            AlterColumn("dbo.Ucenik", "OdeljenjeId", c => c.Int());
+            CreateIndex("dbo.Ucenik", "OdeljenjeId");
+            AddForeignKey("dbo.Ucenik", "OdeljenjeId", "dbo.Odeljenje", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Ucenik", "OdeljenjeId", "dbo.Odeljenje");
+            DropIndex("dbo.Ucenik", new[] { "OdeljenjeId" });
+            AlterColumn("dbo.Ucenik", "OdeljenjeId", c => c.Int(nullable: false));
+            CreateIndex("dbo.Ucenik", "OdeljenjeId");
+            AddForeignKey("dbo.Ucenik", "OdeljenjeId", "dbo.Odeljenje", "Id", cascadeDelete: true);
+        }
+    }
+}
