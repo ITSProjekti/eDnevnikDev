a93715c01167b670839bbe27ef276440c6823de5
Firas <dusan24314@its.edu.rs>
2017-12-19 12:11:44 +0100

    dashboard tromesecja

--- eDnevnikDev/ViewModel/AdminIndexViewModel.cs
+++ eDnevnikDev/ViewModel/AdminIndexViewModel.cs
@@ -11,14 +11,30 @@ namespace eDnevnikDev.ViewModel
 
         public int Devojcice { get; set; }
 
-        public String PocetakPrvogPolugodista { get; set; }
+        public string PocetakPrvogPolugodista { get; set; }
 
-        public String KrajPrvogPolugodista { get; set; }
+        public string KrajPrvogPolugodista { get; set; }
 
-        public String PocetakDrugogPolugodista { get; set; }
+        public string PocetakDrugogPolugodista { get; set; }
 
-        public String KrajDrugogPolugodista { get; set; }
+        public string KrajDrugogPolugodista { get; set; }
 
         public int UkupanBrojUcenika { get; set; }
+
+        public string PocetakPrvogTromesecja { get; set; }
+
+        public string KrajPrvogTromesecja { get; set; }
+
+        public string PocetakDrugogTromesecja { get; set; }
+
+        public string KrajDrugogTromesecja { get; set; }
+
+        public string PocetakTrecegTromesecja { get; set; }
+
+        public string KrajTrecegTromesecja { get; set; }
+
+        public string PocetakCetvrtogTromesecja { get; set; }
+
+        public string KrajCetvrtogTromesecja { get; set; }
     }
 }
